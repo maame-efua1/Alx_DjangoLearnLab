de61b43804de360051a8f1ff31a9da6fdbeb7b5c
eeequartey <efuaessilifua1@gmail.com>
2025-10-30 11:10:24 +0000

    adminView2

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -5,3 +5,4 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
