b29f62f85e3ef6634a43ed39c2558e5f26a591fe
eeequartey <efuaessilifua1@gmail.com>
2025-11-07 11:07:38 +0000

    model relationships1

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query 1: Get all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)
     return books
 
 # Query 2: List all books in a specific library
