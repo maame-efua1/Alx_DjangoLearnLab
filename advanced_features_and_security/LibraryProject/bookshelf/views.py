44b34bbdab9d1ffd94c8b044a5f2894f45167728
eeequartey <efuaessilifua1@gmail.com>
2025-11-13 16:46:50 +0000

    permissions and groups1

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,36 +1,37 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import permission_required
-from .forms import ArticleForm
+from .forms import BookForm
+from .models import Book
 
 @permission_required('relationship_app.can_view', raise_exception=True)
-def article_list(request):
-    articles = Article.objects.all()
-    return render(request, 'relationship_app/article_list.html', {'articles': articles})
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
 
 @permission_required('relationship_app.can_create', raise_exception=True)
-def article_create(request):
+def book_create(request):
     if request.method == 'POST':
-        form = ArticleForm(request.POST)
+        form = BookForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('article_list')
+            return redirect('book_list')
     else:
-        form = ArticleForm()
-    return render(request, 'relationship_app/article_form.html', {'form': form})
+        form = BookForm()
+    return render(request, 'relationship_app/book_form.html', {'form': form})
 
 @permission_required('relationship_app.can_edit', raise_exception=True)
-def article_edit(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    form = ArticleForm(request.POST or None, instance=article)
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    form = BookForm(request.POST or None, instance=book)
     if form.is_valid():
         form.save()
-        return redirect('article_list')
-    return render(request, 'relationship_app/article_form.html', {'form': form})
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_form.html', {'form': form})
 
 @permission_required('relationship_app.can_delete', raise_exception=True)
-def article_delete(request, pk):
-    article = get_object_or_404(Article, pk=pk)
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
     if request.method == 'POST':
-        article.delete()
-        return redirect('article_list')
-    return render(request, 'relationship_app/article_confirm_delete.html', {'article': article})
+        book.delete()
+        return redirect('book_list')
+    return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
