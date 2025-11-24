1b35e934075444e700923f1759e0e9a3ff89c0dd
eeequartey <efuaessilifua1@gmail.com>
2025-11-24 09:18:39 +0000

    Auth & Permissions in REST

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,12 +3,25 @@ from rest_framework import generics
 from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated
+
 
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]   # 🔐 Only logged-in users with a token can access
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+"""
+Authentication:
+- Uses DRF TokenAuthentication
+- Users must obtain a token from /api/get-token/
+
+Permissions:
+- BookList view requires IsAuthenticated
+"""
