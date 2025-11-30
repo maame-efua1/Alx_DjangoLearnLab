b7c01ecf620ebbfcfdc73a12542fa2211b65e96b
eeequartey <efuaessilifua1@gmail.com>
2025-11-30 21:13:32 +0000

    filtering & ordering

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+
 
 
 # 1. LIST VIEW (GET all books)
@@ -54,3 +57,21 @@ class BookDeleteView(generics.DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+ # 🔥 Add Filtering, Searching, Ordering
+    filter_backends = [
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
+    ]
+
+    # Filter by exact match fields
+    filterset_fields = ['title', 'author', 'publication_year']
+
+    # Search in text fields (case-insensitive)
+    search_fields = ['title', 'author__name']
+
+    # Ordering options
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # default ordering
